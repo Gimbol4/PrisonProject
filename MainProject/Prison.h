f1305c6ee36e1c6031b84eb41e9f4527d0f5927d
Gimbol4 <Shishkanov.yd@gmail.com>
2026-06-01 18:08:23 +0300

    .

--- MainProject/Prison.h
+++ MainProject/Prison.h
@@ -5,7 +5,7 @@ using namespace std;
 
 class Prison {
 private:
-	int prisoner;
+	int number_of_prisoner;
 	int* chambers;
 	string guard;
 	string name_of_prison;
@@ -13,30 +13,17 @@ private:
 public:
 
 	Prison();
-
 	Prison(int prs, int chmb);
-
 	Prison(int prs, int chmb, string nmgr, string nop);
-
 	Prison(const Prison& prison);
-
 	~Prison();
-
 	string getName();
-
 	void setName(string nop);
-
 	int* getchamberss();
-
 	void setchamberss(int* chmb, int c);
-
 	int getPrisoner();
-
 	void setPrisoner(int prs);
-
 	string getGuard();
-
 	void setGuard(int grnm);
-
 	string toString();
 };
---
+++ MainProject/Town.h
@@ -0,0 +1,9 @@
+#include "Prison.h"
+
+class Town {
+private:
+	string name;
+	Prison** list;
+	int size;
+public:
+};
